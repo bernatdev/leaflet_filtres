9566d4ed1177dd8788c4826f99e1eb94a55450d6
bernatdev <61184540+bernatdev@users.noreply.github.com>
2020-07-08 15:58:56 +0200

    function render_to_map
    function render_to_map

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var map = L.map('mapid').on('load', onMapLoad).setView([41.400, 2.206], 9);
+var mapaCatalunya = L.map('mapid').on('load', onMapLoad).setView([41.400, 2.206], 9);
 
 //map.locate({setView: true, maxZoom: 17});
-var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mapaCatalunya);
 
-//en el clusters almaceno todos los markers
+//en el cluster almaceno todos los markers
 var markers = L.markerClusterGroup();
 var data_markers = [];
+
 var kind_food_array = [];
 var kind_food_array_isolated = [];
 
@@ -29,7 +30,7 @@ function onMapLoad() {
 
 				kind_food_array.push(item.kind_food.split(",")); // creo un array de arrays, que contienen el tipo de comida de cada restaurant
 			});
-			
+
 			var j;
 			var k;
 			for (j = 0; j < kind_food_array.length; j++) {  // recorro el array de arrays con el tipo de comida de cada restaurant
@@ -38,33 +39,48 @@ function onMapLoad() {
 					let existe = kind_food_array_isolated.find(existe => existe === kind_food_array_members[k]); // averiguo si algún tipo de comidas en el array ya existe en el array final individualizado
 					if (existe == undefined) {
 						kind_food_array_isolated.push(kind_food_array_members[k]); // si el tipo de comida no existe todavía, lo agrego al array final
-						document.getElementById("kind_food_selector").innerHTML += `<option value="${kind_food_array_members[k]}">${kind_food_array_members[k]}</option> <br>`; 
+						document.getElementById("kind_food_selector").innerHTML += `<option value="${kind_food_array_members[k]}">${kind_food_array_members[k]}</option> <br>`;
 
 					}
 				}
 			}
 			console.log(data_markers);
-			console.log(kind_food_array);
-			console.log(kind_food_array_isolated);
+			
+			render_to_map(data_markers,'');
 
 		},
+
 	});
 
-	render_to_map(data_markers, 'all');
+
 }
+
 
 $('#kind_food_selector').on('change', function () {
 	console.log(this.value);
 	render_to_map(data_markers, this.value);
 });
 
+function render_to_map(array_of_restaurants, filter) {
+
+	markers.clearLayers();
+
+	for (restaurant of array_of_restaurants) {
+		//console.log(item.kind_food);
+		if (restaurant.kind_food.includes(filter)) {
+			markers.addLayer(L.marker([restaurant.lat, restaurant.lng])
+				.bindPopup("<b>" + restaurant.name + "</b><br>" + restaurant.address + "<br>" + restaurant.kind_food));
+		}
+	}
+
+	mapaCatalunya.addLayer(markers);
 
 
-function render_to_map(data_markers, filter) {
+
 
 	/*
 	FASE 3.2
-		1) Limpio todos los marcadores
+		1) Limpio todos los marcadores 
 		2) Realizo un bucle para decidir que marcadores cumplen el filtro, y los agregamos al mapa
 	*/
 
